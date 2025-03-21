0413ae4d2e9224ebbd0de45773005ff34e958003
vikram6392 <vikram6392@gmail.com>
2025-03-21 13:26:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,14 +12,14 @@ provider "azurerm" {
 
 }
 
-terraform {
-   backend "azurerm" {
-     resource_group_name   = "workshop"  # The resource group containing the storage account
-     storage_account_name  = "csg100320046a0bf6f6"   # The name of the storage account
-     container_name        = "vikramtest"    # The name of the container to store the state file
-     key                    = "terraform.tfstate" # The name of the state file
-   }
- }
+#terraform {
+#   backend "azurerm" {
+#     resource_group_name   = "workshop"  # The resource group containing the storage account
+#     storage_account_name  = "csg100320046a0bf6f6"   # The name of the storage account
+#     container_name        = "vikramtest"    # The name of the container to store the state file
+ #    key                    = "terraform.tfstate" # The name of the state file
+ #  }
+ #}
 
 
  
